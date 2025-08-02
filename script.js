8bce1953613e02274569de0615844cedb60efec0
Ranadev25 <ranakhandev2025@gmail.com>
2025-08-02 18:12:40 +0600

    some work to responsive

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // ------------ScrollReveal setup-------------
-ScrollReveal().reveal(".home, .header",{
+ScrollReveal().reveal(".home, .header, .servic-h2, .skill-h2, .project-h2",{
   origin:"top",
   reset: true,
   distance: "100px",
   duration: 1000,
   delay: 10,
 })
-ScrollReveal().reveal(".scervice-item1, .homein-h1, .skill-left,  .column-left, .fortfolio-box", {
+ScrollReveal().reveal(".scervice-item1, .homein-h1, .skill-left,  .column-left, .fortfolio-box, .tittle-contract", {
   origin: "left",
   reset: true,
-  distance: "100px",
+  distance: "20px",
   duration: 1000,
   delay: 10,
 })
-ScrollReveal().reveal(".scervice-item3, .about-h2, .skill-right", {
+ScrollReveal().reveal(".scervice-item3, .about-h2", {
   origin: "right",
   reset: true,
-  distance: "100px",
+  distance: "20px",
   duration: 1000,
   delay: 10,
 })
 ScrollReveal().reveal(".column-right", {
   origin: "bottom",
   reset: true,
-  distance: "100px",
+  distance: "20px",
   duration: 1000,
   delay: 10,
 })
@@ -34,14 +34,14 @@ ScrollReveal().reveal(".column-right", {
 const body = document.querySelector("body"),
   header = document.querySelector(".header"),
   themToggle = document.querySelector(".dirk-light"),
-  togglescaech = document.querySelector(".togglescaech"),
-  search = document.querySelector(".search"),
   sliderclose = document.querySelector(".sliderclose"),
   navopen = document.querySelector(".navopen"),
   navber = document.querySelector(".navber"),
   menu = document.querySelectorAll(".menu li a"),
   homesection = document.querySelector(".home"),
   circles = document.querySelectorAll(".circle");
+
+
 let gettheme = localStorage.getItem("theme");
 if (gettheme && gettheme === "light-theme") {
   body.classList.add("light")
@@ -62,11 +62,6 @@ themToggle.addEventListener("click", () => {
   }
 })
 
-
-// ----------------header input scarch icon-----------------------
-togglescaech.addEventListener("click", () => {
-  togglescaech.classList.toggle("active");
-})
 
 //--------------- open and close slide nevbar----------------------
 navopen.addEventListener("click", () => {
@@ -96,3 +91,18 @@ var typed = new Typed('#element', {
 });
 //------------home section end--------------------
 //----------skills circle--------------
+circles.forEach(elem => {
+  let dots = elem.getAttribute("data-dots");
+  let marked = elem.getAttribute("data-percent");
+  let percent = Math.floor(dots * marked / 100);
+  let points = "";
+  let rotate = 360 / dots;
+  for (let i = 0; i < dots; i++){
+    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+  }
+  elem.innerHTML = points;
+  const pointsMarked = elem.querySelectorAll(".points");
+  for (let i = 0; i < percent; i++){
+    pointsMarked[i].classList.add("marked")
+  }
+})
